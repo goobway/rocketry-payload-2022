83893081f650a1915fbb3ce5e29fa49b6392d3be
goobway <calistagreenway@gmail.com>
2022-02-13 16:48:00 -0500

    more on compass

--- payload-arduino/compass/compass.ino
+++ payload-arduino/compass/compass.ino
@@ -11,6 +11,16 @@
 #include <utility/imumaths.h>
 #include <math.h>
 
+/* define variables */
+#define M_PI 3.14159265358979323846 /* pi */
+
+float phiM;             // phi measured
+float thetaM;           // theta measured
+float phiFold = 0;      // phi filtered, old
+float phiFnew;          // phi filtered, new
+float thetaFold = 0;    // phi filtered, old
+float thetaFnew;        // phi filtered, new
+
 /* set time delay between samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
@@ -41,24 +51,32 @@ void loop() {
   /* vector of 3 components saved to "mag" variable, talking to IMU object*/
   imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
 
+  /* calculating measured tilt (XYZ) */
+  phiM = atan2(acc.y(), acc.z())/2/M_PI*360;      // X (roll), degress
+  thetaM = atan2(acc.x(), acc.z())/2/M_PI*360;    // Y (pitch), degress
+
+  /* low-pass filer on tilt/angle measurements */
+  phiFnew = 0.95*phiFold + 0.05*phiM;
+  thetaFnew = 0.95*thetaFold + 0.05*thetaM;
+  
   /* print to serial monitor */
   Serial.print(system);     // system calibration number
   Serial.print(",");
   /* acceleration data */
-  Serial.print(acc.x());    // accel. in x direction
+  Serial.print(acc.x());    // accel. in x direction, m/s^2
   Serial.print(",");
-  Serial.print(acc.y());    // accel. in y direction
+  Serial.print(acc.y());    // accel. in y direction, m/s^2
   Serial.print(",");
-  Serial.print(acc.z());    // accel. in z direction (gravity)
+  Serial.print(acc.z());    // accel. in z direction, m/s^2
   Serial.print(",");
   Serial.print(accCal);     // accel. calibration number
   Serial.print(",");
   /* gyroscope data */
-  Serial.print(gyro.x());   // rotational velocity in x direction
+  Serial.print(gyro.x());   // rotational velocity in x direction, m/s
   Serial.print(",");
-  Serial.print(gyro.y());   // rotational velocity in y direction
+  Serial.print(gyro.y());   // rotational velocity in y direction, m/s
   Serial.print(",");
-  Serial.print(gyro.z());   // rotational velocity in z direction
+  Serial.print(gyro.z());   // rotational velocity in z direction, m/s
   Serial.print(",");
   Serial.print(gyroCal);    // gyro calibration number
   Serial.print(",");
@@ -70,7 +88,20 @@ void loop() {
   Serial.print(mag.z());    // mag. field in z direction
   Serial.print(",");
   Serial.print(magCal);     // mag. calibration number
+  Serial.print(",");
+  /* angle data */
+  Serial.print(phiM);       // phi measured
+  Serial.print(",");
+  Serial.print(thetaM);     // theta measured
+  Serial.print(",");
+  Serial.print(phiFnew);    // phi filtered
+  Serial.print(",");
+  Serial.print(thetaFnew);  // theta filtered
   Serial.print("\n");
+
+  /* update phi and theta filter values */
+  phiFold = phiFnew;
+  thetaFold = thetaFnew;
 
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
